23233ee93f5da085fca7b42632a59ef500e4b8c7
CheaSenghuy <cheasenghuy21@gmail.com>
2024-02-15 11:40:44 +0700

    Change Keys and add crossorigin on Top AuthController

--- src/main/java/com/example/spring_jwt_config/controller/auth/AuthController.java
+++ src/main/java/com/example/spring_jwt_config/controller/auth/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/auth")
+@CrossOrigin
 public class AuthController extends RestApiController {
 
     private final AuthService authService;
